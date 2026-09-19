128dd38765cfce5d9a1d9ecd8e19afd2f3ecf830
Jesús Quintana <jquintana@gmail.com>
2026-09-19 17:41:16 +0200

    v3.18.2: versioned via scripts/versionar.py

--- server.py
+++ server.py
@@ -60,7 +60,7 @@ try:
 except ImportError:
     ARIALIVE_JS = None  # repo checkout: read the file
 
-VERSION = '3.18.1'
+VERSION = '3.18.2'
 
 INSTRUCTIONS = (
     'Accessibility toolkit (WCAG 2.2), multilanguage es/en. '
